2458968a7bcfa554696f4c460fb8d2bad9db7d65
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-01-18 16:52:02 +0530

    problem statement-15:  Which candidate received the highest number of EVM votes in each constituency (Top 10)?

--- query.sql
+++ query.sql
@@ -346,3 +346,28 @@ GROUP BY
     s.State
 ORDER BY 
     s.State;
+
+
+/*
+problem statement-15:  Which candidate received the highest number of EVM votes in each constituency (Top 10)?
+
+ */
+-- query:
+SELECT TOP 10
+    cr.Constituency_Name,
+    cd.Constituency_ID,
+    cd.Candidate,
+    cd.EVM_Votes
+FROM 
+    constituencywise_details cd
+JOIN 
+    constituencywise_results cr ON cd.Constituency_ID = cr.Constituency_ID
+WHERE 
+    cd.EVM_Votes = (
+        SELECT MAX(cd1.EVM_Votes)
+        FROM constituencywise_details cd1
+        WHERE cd1.Constituency_ID = cd.Constituency_ID
+    )
+ORDER BY 
+    cd.EVM_Votes DESC;
+
