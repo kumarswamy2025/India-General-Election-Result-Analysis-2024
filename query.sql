6a96850d8c60dc8bac0ce63444d6044a17b8d6ab
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-01-18 16:42:18 +0530

    problem statement-9:  Add new column field in table partywise_results to get the Party Allianz as NDA, I.N.D.I.A and OTHER

--- query.sql
+++ query.sql
@@ -185,3 +185,60 @@ WHERE
                 'Viduthalai Chiruthaigal Katchi - VCK'
     )
 ORDER BY Seats_Won DESC
+
+
+/*
+problem statement-9:  Add new column field in table partywise_results to get the Party Allianz as NDA, I.N.D.I.A and OTHER 
+
+ */
+-- query:  
+ALTER TABLE partywise_results
+ADD party_alliance VARCHAR(50);
+-- I.N.D.I.A Allianz
+UPDATE partywise_results
+SET party_alliance = 'I.N.D.I.A'
+WHERE party IN (
+    'Indian National Congress - INC',
+    'Aam Aadmi Party - AAAP',
+    'All India Trinamool Congress - AITC',
+    'Bharat Adivasi Party - BHRTADVSIP',
+    'Communist Party of India  (Marxist) - CPI(M)',
+    'Communist Party of India  (Marxist-Leninist)  (Liberation) - CPI(ML)(L)',
+    'Communist Party of India - CPI',
+    'Dravida Munnetra Kazhagam - DMK',	
+    'Indian Union Muslim League - IUML',
+    'Jammu & Kashmir National Conference - JKN',
+    'Jharkhand Mukti Morcha - JMM',
+    'Kerala Congress - KEC',
+    'Marumalarchi Dravida Munnetra Kazhagam - MDMK',
+    'Nationalist Congress Party Sharadchandra Pawar - NCPSP',
+    'Rashtriya Janata Dal - RJD',
+    'Rashtriya Loktantrik Party - RLTP',
+    'Revolutionary Socialist Party - RSP',
+    'Samajwadi Party - SP',
+    'Shiv Sena (Uddhav Balasaheb Thackrey) - SHSUBT',
+    'Viduthalai Chiruthaigal Katchi - VCK'
+);
+-- NDA Allianz
+UPDATE partywise_results
+SET party_alliance = 'NDA'
+WHERE party IN (
+    'Bharatiya Janata Party - BJP',
+    'Telugu Desam - TDP',
+    'Janata Dal  (United) - JD(U)',
+    'Shiv Sena - SHS',
+    'AJSU Party - AJSUP',
+    'Apna Dal (Soneylal) - ADAL',
+    'Asom Gana Parishad - AGP',
+    'Hindustani Awam Morcha (Secular) - HAMS',
+    'Janasena Party - JnP',
+    'Janata Dal  (Secular) - JD(S)',
+    'Lok Janshakti Party(Ram Vilas) - LJPRV',
+    'Nationalist Congress Party - NCP',
+    'Rashtriya Lok Dal - RLD',
+    'Sikkim Krantikari Morcha - SKM'
+);
+-- OTHER
+UPDATE partywise_results
+SET party_alliance = 'OTHER'
+WHERE party_alliance IS NULL;
