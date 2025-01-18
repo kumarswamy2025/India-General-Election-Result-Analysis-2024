4da9c7ef2b1bfe6eefd73f78f4ecac92b9e8674b
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-01-18 16:17:22 +0530

    problem statement-3: What is the total number of seats available for elections in each state by assending order

--- query.sql
+++ query.sql
@@ -14,11 +14,25 @@ problem statement-2: What is the total number of seats available for elections i
  */
 -- query: 
 select
-s.State as STATE_NAME,
-count(sr.Parliament_Constituency) as TOTAL_SEATS_BY_EACH_STATE
+    s.State as STATE_NAME,
+    count(sr.Parliament_Constituency) as TOTAL_SEATS_BY_EACH_STATE
 from
     statewise_results sr
     inner join states s on s.State_ID = sr.State_ID
 group by
     s.State
-
+    /*
+    problem statement-3: What is the total number of seats available for elections in each state by assending order
+    
+     */
+    -- query: 
+SELECT
+    s.State AS STATE_NAME,
+    COUNT(sr.Parliament_Constituency) AS TOTAL_SEATS_BY_EACH_STATE
+FROM
+    statewise_results sr
+    INNER JOIN states s ON s.State_ID = sr.State_ID
+GROUP BY
+    s.State
+ORDER BY
+    TOTAL_SEATS_BY_EACH_STATE asc;
