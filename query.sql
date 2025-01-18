6609ffdfe901852e92442fa39e4cccc285923da3
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-01-18 16:18:43 +0530

    problem statement-3: What is the total number of seats available for elections in each state by descending order

--- query.sql
+++ query.sql
@@ -36,3 +36,20 @@ GROUP BY
     s.State
 ORDER BY
     TOTAL_SEATS_BY_EACH_STATE asc;
+
+/*
+problem statement-3: What is the total number of seats available for elections in each state by descending order 
+
+ */
+-- query: 
+SELECT
+    s.State AS STATE_NAME,
+    COUNT(sr.Parliament_Constituency) AS TOTAL_SEATS_BY_EACH_STATE
+FROM
+    statewise_results sr
+    INNER JOIN states s ON s.State_ID = sr.State_ID
+GROUP BY
+    s.State
+ORDER BY
+    TOTAL_SEATS_BY_EACH_STATE DESC;
+
