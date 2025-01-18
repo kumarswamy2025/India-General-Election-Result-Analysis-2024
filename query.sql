b0fa5042203fd4a714ec46a373b9a529ca9c42ec
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-01-18 15:44:35 +0530

    some changes in problem statement-1 query

--- query.sql
+++ query.sql
@@ -4,5 +4,9 @@ problem statement-1: Total Seats
 */
 
 -- query:
-select distinct count(Parliament_Constituency) from constituencywise_results; 
+select distinct count(Parliament_Constituency) as total_Seats from constituencywise_results; 
 
+/*
+problem statement-2: Total Seats 
+ 
+*/
